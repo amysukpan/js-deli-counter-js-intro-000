475ff3cc96a2a9f1ad8f3f86a2eabd9c3f6ab22f
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-13 02:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length==0) {
     return "There is nobody waiting to be served!"
   }
-  else 
-    return katzDeliLine.shift()
+  else
+    return `Currently serving ${katzDeliLine.shift()}`
 
 }
 
